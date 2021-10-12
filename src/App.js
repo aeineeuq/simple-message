8cad687a2b24a4c5428d07702129f853de5ef6c2
qambatali <q.ambatali@gmail.com>
2021-10-12 18:55:07 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
 
 function App() {
+  const [message, setMessage] = React.useState("JavaScript is so cool.");
+  function handleClick() {
+    setMessage("my new message");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+  <h1>{message}</h1>
+  <button onClick={handleClick}>Update the message!</button>
+  </div>
+
   );
 }
 
